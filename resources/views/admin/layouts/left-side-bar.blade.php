70261bf8b207635259594cbaa9c52787b9416487
Pham Minh Tuan <theloneranger241@gmail,com>
2023-04-21 10:59:18 +0700

    add admin template

---
+++ resources/views/admin/layouts/left-side-bar.blade.php
@@ -0,0 +1,21 @@
+<div class="left-side-menu">
+    <!-- LOGO -->
+    <a href="index.html" class="logo text-center logo-light">
+                    <span class="logo-lg">
+                        <img src="assets/images/logo.png" alt="" height="16">
+                    </span>
+        <span class="logo-sm">
+                        <img src="assets/images/logo_sm.png" alt="" height="16">
+                    </span>
+    </a>
+    <div class="h-100" id="left-side-menu-container" data-simplebar>
+
+        <!--- Sidemenu -->
+        @include('admin.layouts.side-menu')
+        <!-- End Sidebar -->
+
+        <div class="clearfix"></div>
+    </div>
+    <!-- Sidebar -left -->
+
+</div>
---
+++ resources/views/admin/layouts/side-menu.blade.php
@@ -0,0 +1,22 @@
+<ul class="metismenu side-nav">
+    <li class="side-nav-title side-nav-item">Navigation</li>
+
+    <li class="side-nav-item">
+        <a href="javascript: void(0);" class="side-nav-link">
+            <i class="uil-home-alt"></i>
+            <span> Dashboards </span>
+        </a>
+    </li>
+    <li class="side-nav-item">
+        <a href="javascript: void(0);" class="side-nav-link">
+            <i class="uil-home-alt"></i>
+            <span> User </span>
+        </a>
+    </li>
+    <li class="side-nav-item">
+        <a href="javascript: void(0);" class="side-nav-link">
+            <i class="uil-home-alt"></i>
+            <span> Plan </span>
+        </a>
+    </li>
+</ul>
