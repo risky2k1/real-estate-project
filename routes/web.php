9c35cea131763952ded08164ac3156259aee3499
Pham Minh Tuan <theloneranger241@gmail,com>
2023-04-22 14:28:18 +0700

    test and laravel.log clear

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+Route::get('/test',fn()=> view('auth-layouts.login'));
 require __DIR__.'/auth.php';
