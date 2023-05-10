611263253329112a9be373d74f61b91d9dc9e196
Pham Minh Tuan <theloneranger241@gmail,com>
2023-05-10 11:24:59 +0700

    add slider enum

---
+++ app/Enums/ImageTypeEnum.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class ImageTypeEnum extends Enum
+{
+    const User = 1;
+    const Property = 2;
+    const Slider = 3;
+}
