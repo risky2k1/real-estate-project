977c94545845220237998491e7b88e20f5c42c1c
Pham Minh Tuan <theloneranger241@gmail,com>
2023-05-11 09:11:20 +0700

    search properties

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -5,21 +5,28 @@ namespace App\Http\Controllers\Front;
 use App\Enums\ImageTypeEnum;
 use App\Http\Controllers\Controller;
 use App\Models\Image;
+use App\Models\Property;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $sliders = Image::where('type',ImageTypeEnum::Slider)
             ->where('is_active',true)
             ->get();
-        return view('front.layouts.index',[
+
+        $query = Property::query();
+
+        if ($request->has('keyword')){
+            $query->where('name', 'Like', '%'.request('keyword').'%');
+        }
+        $properties = $query->paginate();
+
+        return view('front.properties',[
             'sliders'=>$sliders,
+            'properties'=>$properties,
         ]);
     }
 
-//    public function slider()
-//    {
-//        return view('front.layouts.header');
-//    }
 }
